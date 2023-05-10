3856d9de23229a0e986f2517de64b78a709e8731
rishibhurane <bhuranerishi@gmail.com>
2023-05-10 18:04:23 +0530

    first java program

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    /*
+    * void - (keyword) it doesn't have the return type (anything)
+    * main - Without main you cannot run the code
+    * static - without creating an object of class, main methods gets executed
+    * public - access anywhere(access specifier)
+    * class - template
+    * args - (agrument / parameters) variables
+    * String - Characters passes
+    * [] - stores multiple values (array)
+    * */
+    /*How to Run the java in terminal
+    * 1. write ls
+    * 2. write cd src
+    * 3. write ls
+    * 4. write javac the file name(example HelloWorld.java)
+    * 5. Run the code java HelloWorld
+    * */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
